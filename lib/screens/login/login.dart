e8638270bbb9797d7fe8e52178548370cdd9be03
Patrick Augusto <trick0augusto@gmail.com>
2024-11-17 21:59:24 -0300

    #

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,92 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  State<StatefulWidget> createState() => LoginState();
+}
+
+  class LoginState extends State<Login> {
+  final TextEditingController userController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0XFF29323E),
       body: Center(
-        child: Container(
-          width: 350,
-          padding: const EdgeInsets.all(20),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
           child: Column(
-            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Image.asset(
-                'assets/image/Logo.png',
-                height: 80,
-              ),
-              const SizedBox(height: 30),
-              TextField(
-                decoration: InputDecoration(
-                  labelText: 'Usuário',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
+              const SizedBox(height: 80),
+              const Text(
+                'ILLUMINA',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.yellow,
                 ),
               ),
-              const SizedBox(height: 15),
+              const SizedBox(height: 70),
               TextField(
+                controller: userController,
                 decoration: InputDecoration(
-                  labelText: 'Senha',
+                  hintText: 'Usuário',
+                  hintStyle: TextStyle(color: Colors.grey[400]),
                   filled: true,
-                  fillColor: Colors.white,
+                  fillColor: const Color(0xffffffff),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
                 ),
-                obscureText: true,
               ),
               const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/appbody');
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(Colors.yellow),
-                  foregroundColor: WidgetStateProperty.all(Colors.black),
-                  minimumSize: WidgetStateProperty.all(
-                    const Size(double.infinity, 50),
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Senha',
+                  hintStyle: TextStyle(color: Colors.grey[400]),
+                  filled: true,
+                  fillColor: const Color(0xffffffff),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
                   ),
-                  shape: WidgetStateProperty.all(
-                    RoundedRectangleBorder(
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                ),
+              ),
+              const SizedBox(height: 40),
+              SizedBox(
+                width: 15, // Define a largura do botão
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow,
+                    shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
                     ),
                   ),
-                ),
-                child: const Text(
-                  'Entrar',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
                 ),
               ),
-              const SizedBox(height: 15),
-              GestureDetector(
-                onTap: () {},
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {},
                 child: const Text(
                   'Esqueci a senha',
                   style: TextStyle(color: Colors.white),
                 ),
               ),
-              const SizedBox(height: 30),
-              GestureDetector(
-                onTap: () {},
-                child: RichText(
-                  text: const TextSpan(
+              const Spacer(),
+              TextButton(
+                onPressed: () {},
+                child: const Text.rich(
+                  TextSpan(
                     text: 'Não tem uma conta? ',
                     style: TextStyle(color: Colors.white),
                     children: [
                       TextSpan(
                         text: 'Crie uma',
-                        style: TextStyle(
-                          color: Colors.yellow,
-                          fontWeight: FontWeight.bold,
-                        ),
+                        style: TextStyle(color: Colors.yellow),
                       ),
                     ],
                   ),
+                  textAlign: TextAlign.center,
                 ),
               ),
             ],
@@ -96,3 +114,4 @@ class Login extends StatelessWidget {
     );
   }
 }
+  
