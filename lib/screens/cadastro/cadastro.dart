b3440a954b12a7aae6a410fb781dc55ee80e4165
Bruna Roberta <144182969+brunartt@users.noreply.github.com>
2024-12-04 14:24:53 -0300

    integração da tela cadastro com o banco de dado e ajustes nas funcionalidades

--- lib/screens/cadastro/cadastro.dart
+++ lib/screens/cadastro/cadastro.dart
@@ -1,25 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
+class Cadastro extends StatefulWidget {
+  @override
+  _CadastroState createState() => _CadastroState();
+}
 
-class Cadastro extends StatelessWidget {
+class _CadastroState extends State<Cadastro> {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController = TextEditingController();
+
+  Future<void> _cadastro() async {
+    final email = emailController.text;
+    final username = usernameController.text;
+    final password = passwordController.text;
+    final confirmPassword = confirmPasswordController.text;
+
+    if (password != confirmPassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('As senhas não coincidem')),
+      );
+      return;
+    }
+
+    final user = ParseObject('AppUser')
+      ..set('email', email)
+      ..set('username', username)
+      ..set('password', password);
+
+    var response = await user.save();
+
+    if (response.success) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Cadastro realizado com sucesso!')),
+      );
+      Navigator.pushNamed(context, '/login'); 
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro: ${response.error?.message}')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0XFF29323E),
       body: Center(
         child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const SizedBox(height: 80),
-                  Image.asset(
-                    'assets/image/Logo.png',
-                    height: 80,
-                  ),
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 80),
+              Image.asset(
+                'assets/image/Logo.png',
+                height: 80,
+              ),
               const SizedBox(height: 30),
               TextField(
+                controller: emailController,
                 decoration: InputDecoration(
                   labelText: 'E-mail',
                   filled: true,
@@ -31,6 +74,7 @@ class Cadastro extends StatelessWidget {
               ),
               const SizedBox(height: 15),
               TextField(
+                controller: usernameController,
                 decoration: InputDecoration(
                   labelText: 'Nome de Usuário',
                   filled: true,
@@ -42,6 +86,7 @@ class Cadastro extends StatelessWidget {
               ),
               const SizedBox(height: 15),
               TextField(
+                controller: passwordController,
                 decoration: InputDecoration(
                   labelText: 'Senha',
                   filled: true,
@@ -54,6 +99,7 @@ class Cadastro extends StatelessWidget {
               ),
               const SizedBox(height: 15),
               TextField(
+                controller: confirmPasswordController,
                 decoration: InputDecoration(
                   labelText: 'Confirmar Senha',
                   filled: true,
@@ -66,11 +112,9 @@ class Cadastro extends StatelessWidget {
               ),
               const SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
+                onPressed: _cadastro, // Chama a função _cadastro
                 style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, 
+                  foregroundColor: Colors.black,
                   backgroundColor: Colors.yellow,
                   minimumSize: const Size(double.infinity, 50),
                   shape: RoundedRectangleBorder(
@@ -85,7 +129,7 @@ class Cadastro extends StatelessWidget {
               const SizedBox(height: 15),
               GestureDetector(
                 onTap: () {
-                  Navigator.pushNamed(context, '/'); 
+                  Navigator.pushNamed(context, '/');
                 },
                 child: const Text(
                   'Já tem uma conta? Faça Login',
